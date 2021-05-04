fd8fb7c9b2ad6b60204c8198bf501a92d8b65381
Emma Mattner <emattner1997@gmail.com>
2021-05-04 15:01:37 +0930

    writing questions

--- assets/questions.js
+++ assets/questions.js
@@ -1,25 +1,43 @@
 const questions = [
     {
-        name:
-        type:
+        name: 'projectName',
+        type: 'input',
+        message: 'Whats the name of your project?'
+
+    },
+    {
+        name: 'Description',
+        type: 'input',
+        message: 'Description of your project'
+    },
+    {
+        name: 'tableOfContents',
+        type: 
         message:
 
     },
     {
-        name:
-        type:
-        message:
-    },
-    {
-        name:
-        type:
-        message:
+        name: 'Installation',
+        type: 'input',
+        message: 'Provide installation instructions'
 
     },
     {
-        name:
-        type:
-        message:
+        name: 'Usage',
+        type: 'input',
+        message: 'Input usage infomration'
+
+    },
+    {
+        name: 'Contributing',
+        type: 'input',
+        message: 'Contribution quidelines'
+
+    },
+    {
+        name: 'Tests',
+        type: 'input',
+        message: 'Test instructions'
 
     },
 
